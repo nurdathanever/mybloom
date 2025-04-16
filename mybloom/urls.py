793534f56fce684c6174f604026f486b241898e8
Nurdaulet Duisenov <210103357@stu.sdu.edu.kz>
2025-04-17 01:06:14 +0500

    Admin configuration

--- mybloom/urls.py
+++ mybloom/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import home
+from .views import home, create_admin_user
 from django.conf.urls.static import static
 from django.conf import settings
 
@@ -15,6 +15,7 @@ urlpatterns = [
     path("admin-dashboard/", include("admin_panel.urls")),
     path('yourwish/', include('yourwish.urls')),
     path("cart/", include("cart.urls")),
+    path('create-admin/', create_admin_user),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- mybloom/views.py
+++ mybloom/views.py
@@ -1,4 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+
+def create_admin_user(request):
+    if not User.objects.filter(username="admin").exists():
+        User.objects.create_superuser(
+            username="admin",
+            email="admin@gmail.com",
+            password="mybloomadmin"
+        )
+        return HttpResponse("Admin created.")
+    return HttpResponse("Admin already exists.")
 
 def home(request):
     return render(request, "home.html")
